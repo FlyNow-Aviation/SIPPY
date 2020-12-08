9f0efa4eb5164f9d5e57f64ac62874f3c766d987
Jan Jamaszyk <66419585+JanJamaszyk-FlyNow@users.noreply.github.com>
2020-12-08 10:30:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url="https://github.com/CPCLAB-UNIPI/sippy",
 	packages=setuptools.find_packages(),
-	python_requires="==2.7,>=3.5,<=3.8.5",
+	python_requires="==2.7,>=3.5,<=3.8",
 	install_requires=(
 		"numpy",
 		"scipy",
@@ -28,7 +28,7 @@ setuptools.setup(
 		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
         	"Programming Language :: Python :: 3.7",
-        	"Programming Language :: Python :: 3.8.5",
+        	"Programming Language :: Python :: 3.8",
 		"Operating System :: OS Independent",
 	),
 )
